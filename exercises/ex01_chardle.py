3911c128ffdc3dfc7bb11508e1432370c09009a7
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2023-09-07 13:58:49 -0400

    ex01_chardle complete

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,8 +5,10 @@ user_string: str = input("Enter a 5-character word: ")
 user_character: str = input("Enter a single character: ")
 if (len(user_string) != 5):
     print("Error: Word must contain 5 characters")
+    exit()
 if (len(user_character) != 1):
     print("Error: Character must be a single character")
+    exit()
 print("Searching for " + str(user_character) + " in " + str(user_string))
 if (user_string[0] == user_character):
     print(user_character + " found at index 0")
@@ -23,7 +25,9 @@ if (user_string[3] == user_character):
 if (user_string[4] == user_character):
     print(user_character + " found at index 4")
     counter += 1
+if counter == 0:
+    print("No instances of " + user_character + " found in " + user_string)
 if counter == 1:
-    print("One match was found")
+    print("1 instance of " + user_character + " found in " + user_string)
 else:
-    print(str(counter) + " character matches were found")
+    print(str(counter) + " instances of " + user_character + " found in " + user_string)
