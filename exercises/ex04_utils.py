37aee06e154f2537c70d0068fd50ab5cd84847b6
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2023-10-06 17:24:03 -0400

    Work on ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -3,6 +3,7 @@ __author__ = "730695813"
 
 
 def all(input_list: list[int], num: int) -> bool:
+    """This function if a given input integer is inside of an input list."""
     if len(input_list) == 0:
         return False
     i: int = 0
@@ -14,10 +15,11 @@ def all(input_list: list[int], num: int) -> bool:
 
 
 def max(input_list: list[int]) -> int:
+    """This function takes a list as input and returns the largest value."""
     if len(input_list) == 0:
         raise ValueError("max() arg is an empty list")
-    i: int = 0
-    current_int: int = 0
+    i: int = 1
+    current_int: int = input_list[0]
     while (i < len(input_list)):
         if current_int < input_list[i]:
             current_int = input_list[i]
@@ -27,17 +29,16 @@ def max(input_list: list[int]) -> int:
 
 
 def is_equal(input_list_1: list[int], input_list_2: list[int]) -> bool:
+    """This function checks to see whether or not every value at every index of each of these lists are equal."""
     if len(input_list_1) == 0 and len(input_list_2) == 0:
-        raise ValueError("both is_equal() arguments are empty lists")
+        return True
     elif len(input_list_1) == 0 or len(input_list_2) == 0:
-        raise ValueError("an is_equal() arg is an empty list")
+        return False
     if len(input_list_1) != len(input_list_2):
-        print("is_equal arguments are not equal in length")
-        return
+        return False
     i: int = 0
     while (i < len(input_list_1)):
         if input_list_1[i] != input_list_2[i]:
             return False
         i += 1
     return True
-
