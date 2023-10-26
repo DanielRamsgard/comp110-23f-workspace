cf06013e697d909fbdc15b4083a778bc5790ce6d
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2023-10-25 22:55:37 -0400

    Work on ex06

---
+++ exercises/ex06/dictionary.py
@@ -0,0 +1,180 @@
+"""This code is for ex06 dictionary work."""
+
+__author__ = "730695813"
+
+test_dict = {"Monday": ["Vrinda", "Kaleb"], "Tuesday": ["Alyssa"]}
+empty_dict = {}
+test_list = ["hi", "yo", "yo"]
+
+
+# signature line for invert() function
+def invert(input_dict: dict[str, str]) -> dict[str, str]:
+    """Function that iverts key-value pairs"""
+    # initial setup
+    new_key_list: list[str] = list()
+    repetition_counter: int = 0
+    new_value_list: list[str] = list()
+    new_dict: dict[str, str] = dict()
+
+    # loop over keys
+    for elem in input_dict:
+        new_key_list.append(input_dict[elem])
+
+    # check if any are similar
+    for i in range(0, len(new_key_list), 1):
+        for j in range(0, len(new_key_list), 1):
+            if new_key_list[i] == new_key_list[j]:
+                repetition_counter += 1
+        if repetition_counter >= 2:
+            raise KeyError("You cannot have two input values that are equal")
+        repetition_counter = 0
+    
+    # switch keys and values
+    for elem in input_dict:
+        new_value_list.append(elem)
+
+    # create new dict
+    for i in range(0, len(input_dict), 1):
+        new_dict[new_key_list[i]] = new_value_list[i]
+
+    # return the dict
+    return new_dict
+
+
+# signature line for favorite_color() function
+def favorite_colors(input_dict: dict[str, str]) -> str:
+    """Function that returns the crowd's favorite color."""
+    # initial setup
+    val_list: list[str] = list()
+    repetition_counter: int = 0
+    repetition_list: list[int] = list()
+    current_max: int = 0
+    max_index: int = 0
+    repetition_list_1: list[int] = list()
+
+    # make sure list isn't empty
+    if len(input_dict) == 0:
+        return "None"
+
+    # loop through to get a list of vals
+    for key in input_dict:
+        val_list.append(input_dict[key])
+
+    
+    # check if we have repeats
+    for i in range(0, len(val_list), 1): 
+        for j in range(0, len(val_list), 1):
+            if val_list[i] == val_list[j]:
+                repetition_counter += 1
+
+        # make list of counts
+        repetition_list_1.append(repetition_counter)
+        repetition_counter = 0 
+
+    # initalize max_val
+    max_val: int = repetition_list_1[0]
+
+    # loop through rep_list_1 
+    for val in repetition_list_1:
+        if max_val < val:
+            max_val = val
+
+    if max_val < 2:
+        for elem in input_dict:
+            return input_dict[elem]
+        
+    # update repeition counter to 0
+    repetition_counter = 0
+
+    # check counts of colors in dictionary
+    for i in range(0, len(val_list), 1):
+        for j in range(0, len(val_list)):
+            if val_list[i] == val_list[j]:
+                repetition_counter += 1
+        
+        repetition_list.append(repetition_counter)
+    
+    # 
+    i = 0
+
+    # the index of the max value in repeition_list is the index in val list we need to return
+    current_max = repetition_list[i]
+    for i in range(0, len(repetition_list), 1):
+        if current_max < repetition_list[i]:
+            current_max = repetition_list[i]
+            max_index = i
+    
+    return val_list[max_index]
+
+
+# signature line for count() function
+def count(input_list: list[str]) -> dict[str, int]:
+    """Function that determines how many time something is in a list."""
+    output_dict: dict[str, int] = dict()
+    repetition_counter: int = 0
+    
+    # store keys and intialize values
+    for i in range(0, len(input_list), 1):
+        if not (input_list[i] in output_dict): 
+            output_dict[input_list[i]] = 0
+
+    # store values
+    for i in range(0, len(input_list), 1):
+        for j in range(0, len(input_list), 1):
+            if input_list[i] == input_list[j]:
+                repetition_counter += 1
+        output_dict[input_list[i]] = repetition_counter
+        repetition_counter = 0
+
+    return output_dict
+
+
+# signature line for alphabetizer
+def alphabetizer(input_list: list[str]) -> dict[str, list[str]]:
+    """Function that takes a list and returns letter and list of words starting with letter."""
+    output_dict: dict[str, list[str]] = dict()
+
+    # store keys and intialize values
+    for i in range(0, len(input_list), 1):
+        if not (input_list[i] in output_dict): 
+            current_str = input_list[i]
+            current_str.lower()
+            output_dict[current_str[0]] = list()
+
+    # store words that start with letter
+    for elem in output_dict:
+        for i in range(0, len(input_list), 1):
+            current_str = input_list[i]
+            if elem == current_str[0].lower():
+                output_dict[elem].append(input_list[i])
+    
+    # retrun dict
+    return output_dict
+
+
+# signature line for update_attendence() function
+def update_attendence(input_dict: dict[str, list[str]], day: str, student: str) -> dict[str, list[str]]:
+    """Function that changes a dict based on inputs."""
+    check: int = 0
+    student_list: list[str] = list()
+    student_list.append(student)
+
+
+    # check if day is already in there
+    for elem in input_dict:
+        if elem == day:
+            check = 1
+
+    # if check is 0 store the day and the studen
+    if check == 0:
+        input_dict[day] = student_list
+
+    # if check is 1, day is already in there
+    if check == 1:
+        input_dict[day].append(student)
+
+    # return the dictionary
+    return input_dict
+
+
+print(update_attendence(test_dict, "Tuesday", "Jamie"))
---
+++ lessons/dicts.py
@@ -0,0 +1,44 @@
+"""Practice with dictionaries."""
+
+# constructing a new dictionary
+ice_cream: dict[str, int] = {"chocolate": 12, "vanilla": 8, "strawberry": 5}
+
+# adding a key-value pair to a dictionary
+ice_cream["mint"] = 3
+print(ice_cream)
+
+# altering a value
+ice_cream["mint"] = 4
+print(ice_cream)
+
+# removing a key-value pair
+ice_cream.pop("mint")
+print(ice_cream)
+
+# print chocolate orders and update vanilla to 10
+ice_cream["vanilla"] = 10
+print(ice_cream["chocolate"])
+print(ice_cream)
+
+# len()
+print(len(ice_cream))
+
+# in 
+if "mint" in ice_cream:
+    print(ice_cream["mint"])
+else: 
+    print("mint is not in dict")
+
+if "chocolate" in ice_cream:
+    print(ice_cream["chocolate"])
+else: 
+    print("chocolate is not in dict")
+
+print("\n\n")
+
+# for loop
+for key in ice_cream:
+    print(ice_cream[key])
+
+new_dct_1: dict[int, int] = dict() # or {} for empty dict 
+print(new_dct_1)
