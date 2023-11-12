2affb4079afd21cd9ee3493a9b0a66b8c2b4d2d9
Daniel Ramsgard <Daniel.Ramsgard@unc.edu>
2023-11-12 08:39:25 -0500

    Comp

--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -10,7 +10,7 @@ class Point:
     x: float
     y: float
 
-    def __init__(self, x_init: float, y_init: float):
+    def __init__(self, x_init: float = 0.0, y_init: float = 0.0):
         """Constructor for Point class."""
         # set inputs to class attributes
         self.x = x_init
@@ -33,3 +33,35 @@ class Point:
 
         # return a new point
         return new_point
+
+    def __str__(point: Point) -> str:
+        """Prints values of x and y."""
+        return f"x: {point.x}; y: {point.y}"
+    
+    def __mul__(point: Point, factor: int | float) -> Point:
+        """Returns new point with scaled variables."""
+        # initial setup
+        new_point: Point = Point(1.0, 1.0)
+        point.x *= factor
+        point.y *= factor
+        new_point.x = point.x
+        new_point.y = point.y
+
+        # return new point
+        return new_point
+    
+    def __add__(point: Point, factor: int | float) -> Point:
+        """Adds a value to both x and y."""
+        # initial setup
+        new_point: Point = Point(0.0, 0.0)
+        point.x += factor
+        point.y += factor
+        new_point.x = point.x
+        new_point.y = point.y
+
+        # return the new point
+        return new_point
+
+
+my_p: Point = Point(1.0, 2.0)
+print(my_p + 7.0)
